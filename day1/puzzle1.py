24670c7ef597bb2f7148c30b3f839d53c60751bb
Robbie de Jonge <robbie.dejonge@captin.nl>
2025-12-01 08:30:46 +0100

    1 gold star

---
+++ day1/puzzle1.py
@@ -0,0 +1,37 @@
+f = open('day1/input.txt', 'r')
+lines = f.read().split('\n')
+
+
+dial = 50
+n_zeroes = 0
+
+
+EXAMPLE = [
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+]
+
+
+for l in lines:  # or: `for l in EXAMPLE`
+    direction = l[0]
+    magnitude = int(l[1:])
+
+    if direction.casefold() == "l":
+        dial = (dial - magnitude) % 100
+
+    if direction.casefold() == "r":
+        dial = (dial + magnitude) % 100
+
+    if not dial:
+        n_zeroes +=1
+
+
+print(n_zeroes)
